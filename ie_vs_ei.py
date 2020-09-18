c3e427f721725325b4b1fac2aceebdb44aa1e6ac
Mark Morreale <markemorreale@gmail.com>
2020-09-18 15:50:42 -0500

    Added todo comment to test VSC

--- ie_vs_ei.py
+++ ie_vs_ei.py
@@ -9,3 +9,6 @@ import pronouncing
 word = "perceive"
 print("The CMUDict Pronouncing Dictionary will give the ARPABET pronunciation for the string assigned to 'word':", word)
 print(pronouncing.phones_for_word(word))
+
+#TODO: add list of all CMUDict words containing the sound IY @markmorreale
+
